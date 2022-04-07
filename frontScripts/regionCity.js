fe0953d385d0afebfd15c8ee377618dba9209155
André Umbert <andrupa.10@gmail.com>
2022-04-07 09:20:19 -0300

    starting region/city
    starting region/city

---
+++ frontScripts/regionCity.js
@@ -0,0 +1,10 @@
+//Que? Quiero hacer un array de objetos con otros objetos dentro.
+//Como? Usando array multidimensinal, pero primero tengo que definir desde abajo hacia arriba, es decir desde ciudades hasta regiones y luego incluirlas dentro
+//Para que? Para tener todas las regiones, paises y ciudades debidamente y hacer una especia de diagrama de arbol
+const americaCities = ["New York", "Las Vegas", "Orlando", "Los Angeles"];
+const canadaCities = ["Toronto", "Ottawa", "Vancouver", "Quebec"];
+const mexicoCities = ["Ciudad de Mexico", "Guadalajara", "Monterrey","Tijuana"];
+
+const northAmericaRegion = [
+    {"Estados Unidos"}
+]
