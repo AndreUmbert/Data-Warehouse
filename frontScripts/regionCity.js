380a8f64674a2280cf7149c8debe6aa47c608d96
ANDRUPA <andrupa.10@gmail.com>
2022-04-18 01:03:26 -0300

    add region and countries
    axios functions was created

--- frontScripts/regionCity.js
+++ frontScripts/regionCity.js
@@ -1,15 +1,25 @@
 //Que? Quiero hacer un array de objetos con otros objetos dentro (Nested Array).
 //Como? Usando array multidimensinal, pero primero tengo que definir desde abajo hacia arriba, es decir desde ciudades hasta regiones y luego incluirlas dentro
 //Para que? Para tener todas las regiones, paises y ciudades debidamente y hacer una especia de diagrama de arbol
-//Por que? Al usar un nested array de objetos, esto me permite poder acceder a cada uno de los valores de los objetos que cree.
 
+// const { default: axios } = require("axios");
+
+//Por que? Al usar un nested array de objetos, esto me permite poder acceder a cada uno de los valores de los objetos que cree.
+const blur = document.getElementById("blurSection");
 const token = localStorage.getItem("token");
-// console.log(token);
 const config = { headers: { Authorization: `Bearer ${token}` } };
 console.log(config);
-const regionCity = document.getElementById("regionC4ity");
+const regionCity = document.getElementById("regionCity");
 const regionUl = document.getElementById("regionCityUl");
-
+const addRegionCityButton = document.getElementById("addRegionCityButton");
+const addRegionFormContainer = document.getElementById(
+    "addRegionFormContainer"
+);
+const inputRegion = document.getElementById("inputRegion");
+let formControl = document.getElementById("formControl");
+const regionAddButton = document.getElementsByClassName("regionAddButton");
+let contador = 0;
+let indexCountry = 0;
 // const americaCities = [
 //     { name: "New York", value: 1 },
 //     { name: "Las Vegas", value: 2 },
@@ -72,20 +82,14 @@ const regionUl = document.getElementById("regionCityUl");
 
 // regions[0].northAmericaCountries.forEach(function (e) {console.log(e.name)}
 
-
-
 // Get regions from server
-
-
 const getDemographics = async () => {
-
     let paises = await axios.get(
         "http://localhost:3000/country/dashboard",
         config
     );
 
     let cities = await axios.get("http://localhost:3000/city/dashboard", config);
-
 
     for (let pIndex = 0; pIndex < paises.data.length; pIndex++) {
         paises.data[pIndex].cities = [];
@@ -96,11 +100,8 @@ const getDemographics = async () => {
         }
     }
 
-    getAndShowRegions(paises)
-
+    getAndShowRegions(paises);
 };
-
-
 
 const getAndShowRegions = async (paises) => {
     let regiones = await axios.get(
@@ -112,24 +113,19 @@ const getAndShowRegions = async (paises) => {
         regiones.data[rIndex].countries = [];
         for (let pIndex = 0; pIndex < paises.data.length; pIndex++) {
             if (regiones.data[rIndex].id == paises.data[pIndex].regionId) {
-                regiones.data[rIndex].countries.push(
-                    paises.data[pIndex]
-                );
+                regiones.data[rIndex].countries.push(paises.data[pIndex]);
             }
         }
     }
 
-    showRegions(regiones.data)
-
+    showRegions(regiones.data);
 };
-
-
 
 getDemographics();
 
 function showRegions(regions) {
     for (let region of regions) {
-        console.log(region);
+        // console.log(region);
         const regionEntry = document.createElement("div");
         regionEntry.setAttribute("class", "regionEntry");
         regionUl.appendChild(regionEntry);
@@ -158,8 +154,12 @@ function showRegions(regions) {
         regionAddCountryButton.appendChild(regionAddCountryButtonText);
         regionPlusAddButton.appendChild(regionAddCountryButton);
         regionUl.appendChild(regionEntry);
+        regionAddCountryButton.setAttribute("countryNumber", indexCountry);
+        indexCountry = indexCountry + 1;
+        regionAddCountryButton.setAttribute("onclick", "createCountry(this)");
         for (let country of region.countries.values()) {
             // console.log(country);
+            console.log(region.countries);
             const countryEntry = document.createElement("div");
             countryEntry.setAttribute("class", "countryEntry");
             const countryNameUl = document.createElement("ul");
@@ -219,4 +219,103 @@ function showRegions(regions) {
             }
         }
     }
+}
+
+// const demoForLoop = (topLevel, bottomLevel, flag) => {
+//     for (let i = 0; i < topLevel.data.length; i++) {
+//         flag ? (topLevel.data[i].cities = []) : (topLevel.data[i].countries = []);
+//         for (let j = 0; j < bottomLevel.data.length; j++) {
+//             if (
+//                 flag
+//                     ? topLevel.data[i].id == bottomLevel.data[j].countryId
+//                     : topLevel.data[i].id == bottomLevel.data[j].regionId
+//             ) {
+//                 flag
+//                     ? topLevel.data[i].cities.push(bottomLevel.data[j])
+//                     : topLevel.data[i].countries.push(bottomLevel.data[j]);
+//             }
+//         }
+//     }
+//     return topLevel;
+// };
+
+//Add Region Button:
+addRegionCityButton.addEventListener("click", () => {
+    contador = contador + 1;
+    if (!(contador % 2 == 0)) {
+        addRegionFormContainer.style.display = "block";
+        addRegionFormContainer.style.position = "relative";
+        addRegionFormContainer.style.width = "20%";
+        addRegionFormContainer.style.margin = "0 0 0 75.8%";
+        addRegionFormContainer.style.border = "1px solid rgb(85, 85, 209)";
+        inputRegion.style.display = "flex";
+    } else {
+        addRegionFormContainer.style.display = "none";
+    }
+    console.log(contador);
+});
+
+function createRegion() {
+    console.log(formControl.value);
+    axios.post(
+        "http://localhost:3000/region/create",
+        { regionName: formControl.value },
+        config
+    );
+    location.reload();
+}
+
+//Add Country Button:
+function createCountry(element) {
+    console.log(element.getAttribute("countrynumber"));
+    const blurDiv = document.createElement("blurDiv");
+    blur.appendChild(blurDiv);
+    blurDiv.style.position = "absolute";
+    blurDiv.style.width = "100%";
+    blurDiv.style.height = "100%";
+    blurDiv.style.top = "0";
+    regionCity.style.filter = "blur(8px)";
+    const inputCountryContainer = document.createElement("Container");
+    blurDiv.appendChild(inputCountryContainer);
+    inputCountryContainer.style.position = "absolute";
+    inputCountryContainer.style.display = "flex";
+    inputCountryContainer.style.flexDirection = "column";
+    inputCountryContainer.style.alignItems = "center";
+    inputCountryContainer.style.width = "50%";
+    inputCountryContainer.style.margin = " 0 0 0 25%";
+    inputCountryContainer.style.backgroundColor = "rgb(130, 174, 255)";
+    inputCountryContainer.style.height = "15vw";
+    inputCountryContainer.style.top = "25vw";
+    inputCountryContainer.style.border = "1px solid rgb(85, 85, 209)";
+    inputCountryContainer.style.borderRadius = "1.5vw";
+    const inputCountryText = document.createElement("p");
+    inputCountryText.appendChild(document.createTextNode("Create Country"));
+    inputCountryContainer.appendChild(inputCountryText);
+    const inputCountry = document.createElement("input");
+    inputCountry.setAttribute("type", "text");
+    inputCountry.setAttribute("placeholder", "Country Name");
+    inputCountryContainer.appendChild(inputCountry);
+    const buttonsContainer = document.createElement("div");
+    buttonsContainer.setAttribute("id", " buttonsContainer");
+    inputCountryContainer.appendChild(buttonsContainer);
+    const createButton = document.createElement("button");
+    createButton.setAttribute("id", "createButton");
+    buttonsContainer.appendChild(createButton);
+    createButton.setAttribute("type", "button");
+    createButton.appendChild(document.createTextNode("Create"));
+    const cancelButton = document.createElement("button");
+    cancelButton.setAttribute("id", "cancelButton");
+    buttonsContainer.appendChild(cancelButton);
+    cancelButton.setAttribute("type", "button");
+    cancelButton.appendChild(document.createTextNode("Close"));
+    cancelButton.addEventListener("click", () => {
+        blur.removeChild(blurDiv);
+        regionCity.style.filter = "none";
+    });
+    createButton.addEventListener("click", async () => {
+        let reg = await axios.get("http://localhost:3000/region/dashboard", config);
+        // console.log(reg.data[element.getAttribute("countrynumber")].id);
+        let regPost = await axios.post("http://localhost:3000/country/create", { "name": inputCountry.value, "regionId": reg.data[element.getAttribute("countrynumber")].id }, config);
+        location.reload();
+    });
 }
